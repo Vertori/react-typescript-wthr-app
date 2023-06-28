1d87793ddf0cd3239cca1c2b094f8c62d76884f2
Vertori <115083330+Vertori@users.noreply.github.com>
2023-06-28 14:27:33 +0200

    vite setup, tailwind setup

---
+++ src/App.tsx
@@ -0,0 +1,11 @@
+import "./App.css";
+
+function App() {
+  return (
+    <main className="flex justify-center items-center h-[100vh] w-full bg-gradient-to-br from-sky-300 via-sky-200 to-white">
+      Hello!
+    </main>
+  );
+}
+
+export default App;
