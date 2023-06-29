c6c1ee597e245670831f238103f5a00324b36185
Vertori <115083330+Vertori@users.noreply.github.com>
2023-06-29 19:27:35 +0200

    first api call

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,33 @@
+import { ChangeEvent, useEffect, useState } from "react";
 import "./App.css";
+import axios from "axios";
+import { optionType } from "./types";
 
-function App() {
+function App(): JSX.Element {
+  const [term, setTerm] = useState<string>("");
+  const [options, setOptions] = useState<[]>([]);
+
+  const getSearchOptions = async (value: string) => {
+    await axios
+      .get(
+        `http://api.openweathermap.org/geo/1.0/direct?q=${value.trim()}&limit=5&appid=${
+          import.meta.env.VITE_API_KEY
+        }`
+      )
+      .then((response) => setOptions(response.data));
+  };
+
+  const handleInputChange = (e: ChangeEvent<HTMLInputElement>) => {
+    const value = e.target.value.trim();
+    setTerm(value);
+    if (value === "") return;
+    getSearchOptions(value);
+  };
+
+  const handleOptionSelect = (option: optionType) => {
+    console.log(option.name);
+  };
+
   return (
     <main className="flex justify-center items-center h-[100vh] w-full bg-gradient-to-br from-sky-400 via-rose-400 to-lime-400">
       <section className="w-full md:max-w-[500px] p-4 flex flex-col text-center items-center justify-center md:px-10 lg:p-24 h-full lg:h-[500px] bg-white bg-opacity-20 backdrop-blur-lg drop-shadow-lg rounded text-zinc-700">
@@ -10,12 +37,25 @@ function App() {
         <p className="text-sm mt-2">
           Enter below a place you would like to know the weather of
         </p>
-        <div className="flex mt-10 md:mt-4">
+        <div className="relative flex mt-10 md:mt-4">
           <input
             type="text"
-            value={"value"}
+            value={term}
             className="px-2 py-1 rounded-l-md border-2 border-white"
+            onChange={handleInputChange}
           />
+          <ul className="absolute top-9 bg-white ml-1 rounded-b-md">
+            {options.map((option: optionType, index: number) => (
+              <li key={option.name + "-" + index}>
+                <button
+                  className="text-left text-sm w-full hover:bg-zinc-700 hover:text-white px-2 py-1 cursor-pointer"
+                  onClick={() => handleOptionSelect(option)}
+                >
+                  {option.name}
+                </button>
+              </li>
+            ))}
+          </ul>
           <button className="rounded-r-md border-2 border-zinc-100 hover:border-zinc-500 hover:text-zinc-500 text-zinc-100 px-2 py-1 cursor-pointer">
             Search
           </button>
